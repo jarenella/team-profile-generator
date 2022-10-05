ed8f2beb84d2e75cc8a8ef0da29954388eb7dfb9
jarenella <jamesa122333@gmail.com>
2022-10-05 15:06:50 -0700

    engineer.js Engineer class creator now extends Employee class creator

--- class-lib/engineer.js
+++ class-lib/engineer.js
@@ -1,8 +1,9 @@
-class Engineer {
-    constructor(name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
+const Employee = require("./employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, gitHub) {
+        super(name, id, email);
+        this.gitHub = gitHub;
         this.role = "Engineer";
     }
     getName() {
@@ -17,6 +18,13 @@ class Engineer {
     getRole() {
         return this.role
     }
+    getGitHub() {
+        return this.gitHub
+    }
 }
+
+const engie = new Engineer("namesie", 4, "email@thingy", "gittyhubbusername");
+
+console.log(engie);
 
 module.exports = Engineer;
