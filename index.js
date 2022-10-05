b8f621975898209423036f8cb0a4105ec11fc126
jarenella <jamesa122333@gmail.com>
2022-10-05 16:30:24 -0700

    built basic prompt in index.js for the menu and commented out lines used for testing in engineer.js. Also created a basic generateHTML function which currently does nothing in generatehtml.js

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const fs = require('fs');
+const inquirer = require('inquirer');
 const Employee = require("./lib/employee.js");
 const Engineer = require("./lib/engineer.js");
 const Intern = require("./lib/intern.js");
 const Manager = require("./lib/manager.js");
+
+inquirer.prompt([
+    {
+        type: "list",
+        name: "menu",
+        message: "Would you like to add a user?",
+        choices: ['Yes, add user', 'No, end application']
+    }
+])
+.then(val => {
+    if (val == 'Yes, add user') {
+        //ask other questions
+    }
+    if (val == 'No, end application') {
+        // end app
+    }
+})
+
+
+
+
+
+
